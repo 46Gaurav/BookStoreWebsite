f8850b07872691137c79aeae959b07af837c70c8
46Gaurav <83156026+46Gaurav@users.noreply.github.com>
2021-10-16 20:57:09 +0530

    Create Users.java

---
+++ Users.java
@@ -0,0 +1,53 @@
+package com.bookstore.entity;
+import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+@Entity
+public class Users {
+
+ 
+		private int userId;
+		private String email;
+		private String fullname;
+		private String password;
+	    @Column(name="user_id")
+	    @Id
+	    @GeneratedValue(strategy=GenerationType.IDENTITY)
+		public int getUserId() {
+			return userId;
+		}
+
+		public void setUserId(int userId) {
+			this.userId = userId;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+	    @Column(name="full_name")
+		public String getFullname() {
+			return fullname;
+		}
+
+		public void setFullname(String fullname) {
+			this.fullname = fullname;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+	}
+
+
+
+
